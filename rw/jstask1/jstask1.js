783d8d37728995439382597e241ab8b2b77b3004
lmtdlb <1004908275@qq.com>
2019-05-09 20:20:51 +0800

    qqq

---
+++ rw/jstask1/jstask1.js
@@ -0,0 +1,60 @@
+var timer;
+
+function bbox() {
+    //获取颜色数组
+   /* var colors = [];
+    while (colors.length < 3){
+        var color = "#";
+        for (var i = 0; i < 3; i++) {
+            var n = Math.round((Math.random() * 255));
+            if (n <= 16) {
+                color += "0";
+                color += n.toString(16);
+            } else {
+                color += Math.round((Math.random() * 255)).toString(16);
+            }
+        }
+        colors.push(color);
+    }*/
+   /*var colors = [];*/
+    function colors(){
+        var r = Math.floor(Math.random()*256);
+        var g = Math.floor(Math.random()*256);
+        var b = Math.floor(Math.random()*256);
+        return "rgb("+r+","+g+","+b+")";
+    }
+//为div赋值颜色
+     var myBox = document.getElementsByClassName("box");
+   /* console.log(myBox);*/
+    for (var i=0;i<9;i++){
+        myBox[i].style.backgroundColor = "orange";
+    }
+    myBox[sort()[1]].style.backgroundColor = colors();
+    myBox[sort()[2]].style.backgroundColor = colors();
+    myBox[sort()[3]].style.backgroundColor = colors();
+}
+function myfunction(){
+    clearInterval(timer);
+     timer = setInterval(bbox ,1000);
+}
+
+
+    function sort() {
+        var arr = [0,1,2,3,4,5,6,7,8];
+        var newArr = [];
+        for(var i = 0, len = arr.length; i < len; i++) {
+            var j = Math.floor(Math.random() * (len - i));
+            newArr[i] = arr[j];
+            arr.splice(j, 1)
+        }
+        return newArr;
+    }//洗牌算法
+    console.log(sort());
+
+function end() {
+    clearInterval(timer);
+    var bg = document.getElementsByClassName("box");
+    for (var i=0;i<9;i++){
+        bg[i].style.backgroundColor = "orange";
+    }
+}
