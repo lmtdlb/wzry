cbc25951832e0548b54e22292f2d68d8418050c8
lmtdlb <1004908275@qq.com>
2019-05-31 15:04:32 +0800

    qqq

--- rw/jstask2-4/js/jstask4-1.js
+++ rw/jstask2-4/js/jstask4-1.js
@@ -1,5 +1,7 @@
 var key = sessionStorage.getItem("key");//获取到浏览器中的值
 var array = key.split(",");//将字符串拆分为数组
+var player = [];
+var peopleCounting = [[]];
 for(i = 1;i <= array.length;i++){
         $("main").append(`
             <div class="box">
@@ -7,7 +9,10 @@ for(i = 1;i <= array.length;i++){
                     <div class="num"><span>${i + "号"}</span></div>
              </div>
         `);
+    player.push(({name: array[i-1], state: "live",num: i + "号",whoKill:"none"}));
 }
+sessionStorage.setItem("player", JSON.stringify(player));
+sessionStorage.setItem("people", JSON.stringify(peopleCounting));
 function start() {
     window.location.href="judge text.html";//js页面跳转
 }
