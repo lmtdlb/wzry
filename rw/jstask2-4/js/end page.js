32826a4f17b01b9b73da6e6881b0e2f710489424
lmtdlb <1004908275@qq.com>
2019-06-02 16:47:52 +0800

    qqq

---
+++ rw/jstask2-4/js/end page.js
@@ -0,0 +1,33 @@
+var peopleCounting = JSON.parse(sessionStorage.getItem("people"));
+var key = sessionStorage.getItem("key");
+var array = key.split(",");
+var player = JSON.parse(sessionStorage.getItem("player"));
+var x = 0;
+var y = 0;
+window.onload = function() {
+    for(i = 1;i <= array.length;i++) {
+        if((player[i-1].name == "杀手") && (player[i-1].state == "live")){
+            x++;
+        }
+        if((player[i-1].name == "平民") && (player[i-1].state == "live")){
+            y++;
+        }
+    }
+    for (i = 1; i < peopleCounting.length; i++) {
+        $(".day-num").append(` 
+            <p>第${i}天</p>
+            <p class="killed${i}"></p>
+            <p class="writ${i}"></p>
+    
+        `);
+        $(".killed" + i).append(`
+            黑夜：${peopleCounting[i - 1][0].num}号被杀死，真实身份是${peopleCounting[i - 1][0].identity}
+        `);
+        $(".writ" + i).append(`
+            白天：${peopleCounting[i - 1][1].num}号被投死，真实身份是${peopleCounting[i - 1][1].identity}
+        `);
+    }
+    $(".player").append(` 
+        <p>杀&nbsp;&nbsp;手${x}人&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;平&nbsp;&nbsp;民${y}人</p>
+    `);
+}
